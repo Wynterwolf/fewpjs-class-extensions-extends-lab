88dc05a241a8cde264f6cac7d44b53ee89d37100
Samantha Jordan <shinyguns@gmail.com>
2021-04-12 17:03:12 -0500

    done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+class Polygon {
+    constructor(sides) {
+        // this.array = array
+        this.sides = sides
+    }
+    get countSides(){
+        return this.sides.length
+    }
+    get perimeter(){
+        const sides = this.sides
+        let sum = 0
+        sides.forEach(function(side){
+            sum += side
+        })
+        return sum
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        const a = this.sides[0]
+        const b = this.sides[1]
+        const c = this.sides[2]
+        return (this.countSides === 3 && (a+b > c && a+c > b && b+c > a) )
+    }
+}
+
+class Square extends Polygon {
+    get isValid(){
+        const a = this.sides[0]
+        const b = this.sides[1]
+        const c = this.sides[2]
+        const d = this.sides[3]
+        return (this.countSides === 4 && (a+b === c+d))
+    }
+    get area(){
+        const a = this.sides[0]
+        const b = this.sides[1]
+        return (a * b)
+    }
+}
